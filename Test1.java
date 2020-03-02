ebb5a4a7c096bd1e60b30e711661f253f39c2697
pankajareddy69416 <pankaja69416@gmail.com>
2020-03-02 12:30:51 +0530

    second stage

---
+++ Test1.java
@@ -0,0 +1,23 @@
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		int total=0;
+		int avg=0;
+	int a[]= {1,2,3,4,5};
+	for(int i=0;i<a.length;i++)
+	{	
+		total=total+a[i];
+	}
+	
+	avg=total/a.length;
+	
+	System.out.println(avg);
+	}
+		
+		
+	}
+	
+	
+
