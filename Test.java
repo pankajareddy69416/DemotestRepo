d756ea16c8324845371875a1e81ccaf184eda6fe
pankajareddy69416 <pankaja69416@gmail.com>
2020-03-02 12:25:28 +0530

    first stage

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		System.out.println(a.length);
+		
+		for(int i=a.length-1;i>=0;i--)
+		{
+			System.out.println(a[i]);
+			//System.out.println(a.length);
+		}
+			
+	}
+
+}
